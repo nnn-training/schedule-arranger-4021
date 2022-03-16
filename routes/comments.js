113601ed559dfb4b0c12ece8753461f73793eee9
yuhsug <whitetime47@gmail.com>
2022-03-16 12:02:38 +0900

    promise-chain -> async/await

--- routes/comments.js
+++ routes/comments.js
@@ -7,18 +7,17 @@ const Comment = require('../models/comment');
 router.post(
   '/:scheduleId/users/:userId/comments',
   authenticationEnsurer,
-  (req, res, next) => {
+  async (req, res, next) => {
     const scheduleId = req.params.scheduleId;
     const userId = req.params.userId;
     const comment = req.body.comment;
 
-    Comment.upsert({
+    await Comment.upsert({
       scheduleId: scheduleId,
       userId: userId,
       comment: comment.slice(0, 255)
-    }).then(() => {
-      res.json({ status: 'OK', comment: comment });
     });
+    res.json({ status: 'OK', comment: comment });
   }
 );
 
