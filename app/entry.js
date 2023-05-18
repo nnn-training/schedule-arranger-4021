39e671cb7ed1e5c38720854771c2b5ee8371362f
hsjoihs <hirotaka_satou@dwango.co.jp>
2023-05-18 16:07:46 +0900

    jQuery を取り除いた版

--- app/entry.js
+++ app/entry.js
@@ -1,34 +1,46 @@
 'use strict';
-import $ from 'jquery';
-
-$('.availability-toggle-button').each((i, e) => {
-  const button = $(e);
-  button.on('click', () => {
-    const scheduleId = button.data('schedule-id');
-    const userId = button.data('user-id');
-    const candidateId = button.data('candidate-id');
-    const availability = parseInt(button.data('availability'));
+const toggle_buttons = document.querySelectorAll('.availability-toggle-button');
+toggle_buttons.forEach(button => {
+  button.addEventListener('click', async () => {
+    const scheduleId = button.getAttribute('data-schedule-id');
+    const userId = button.getAttribute('data-user-id');
+    const candidateId = button.getAttribute('data-candidate-id');
+    const availability = parseInt(button.getAttribute('data-availability'));
     const nextAvailability = (availability + 1) % 3;
-    $.post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
-      { availability: nextAvailability },
-      (data) => {
-        button.data('availability', data.availability);
-        const availabilityLabels = ['欠', '？', '出'];
-        button.text(availabilityLabels[data.availability]);
-      });
+    const url = `/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`;
+    const data = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ availability: nextAvailability })
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    });
+    button.setAttribute('data-availability', data.availability);
+    const availabilityLabels = ['欠', '？', '出'];
+    button.textContent = availabilityLabels[data.availability];
   });
 });
 
-const buttonSelfComment = $('#self-comment-button');
-buttonSelfComment.on('click', () => {
-  const scheduleId = buttonSelfComment.data('schedule-id');
-  const userId = buttonSelfComment.data('user-id');
+const buttonSelfComment = document.querySelector('#self-comment-button');
+buttonSelfComment.addEventListener('click', async () => {
+  const scheduleId = buttonSelfComment.getAttribute('data-schedule-id');
+  const userId = buttonSelfComment.getAttribute('data-user-id');
   const comment = prompt('コメントを255文字以内で入力してください。');
   if (comment) {
-    $.post(`/schedules/${scheduleId}/users/${userId}/comments`,
-      { comment: comment },
-      (data) => {
-        $('#self-comment').text(data.comment);
-      });
+    const url = `/schedules/${scheduleId}/users/${userId}/comments`;
+    const data = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ comment: comment })
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    });
+    document.querySelector('#self-comment').textContent = data.comment;
   }
 });
